e2892a78c7e28904a5d375018a16694f85048c73
Brued <Bruedbes@gmail.com>
2024-07-04 14:04:04 -0300

    add ex3

--- ex.py
+++ ex.py
@@ -18,12 +18,22 @@
 # Temperatura > 26°C é 'Alta'
 
 
-temp = float(input ("Valor da temperatura º C: "))
+# temp = float(input ("Valor da temperatura º C: "))
 
-if temp < 18 :
-    print (f"A temperatura {temp} é baixa.")
-elif  18 <= temp <= 26 :
-    print(f"A temperatura {temp} é normal.")
-else:
-    print (f"A temperatura {temp} é alta.")
+# if temp < 18 :
+#     print (f"A temperatura {temp} é baixa.")
+# elif  18 <= temp <= 26 :
+#     print(f"A temperatura {temp} é normal.")
+# else:
+#     print (f"A temperatura {temp} é alta.")
 
+# Exercício 3: Filtragem de Logs por Severidade
+# Você está analisando logs de uma aplicação e precisa filtrar mensagens com severidade 'ERROR'. 
+# Dado um registro de log em formato de dicionário como 
+# log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}, 
+# escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
+
+log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}
+
+if log['level'] =='ERROR':
+    print(log['message'])
